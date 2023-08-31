eed5e605ae2a1389907408e1f5141718a4a4f7ee
Дмитрий Солодов <dmitrijsolodov@MacBook-Pro-Dmitrij.local>
2023-08-31 11:45:44 +0300

    edit basename

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,4 +2,4 @@ import { createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes.tsx';
 
 
-export const router = createBrowserRouter(routes);
+export const router = createBrowserRouter(routes,{basename: '/calendar'});
